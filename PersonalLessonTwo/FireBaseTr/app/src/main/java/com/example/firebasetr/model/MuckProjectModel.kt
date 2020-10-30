471227e9807a525f3f0a3c5e2e81bf13c8a65f8a
Loner123 <dlwlstjq410@gmail.com>
2020-10-31 02:51:45 +0900

    [ADD] 우리 플젝팀에 맞게 예제 변경

--- PersonalLessonTwo/FireBaseTr/app/src/main/java/com/example/firebasetr/model/MuckProjectModel.kt
+++ PersonalLessonTwo/FireBaseTr/app/src/main/java/com/example/firebasetr/model/MuckProjectModel.kt
@@ -5,39 +5,37 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ServerTimestamp
 import kotlinx.android.parcel.Parcelize
 
-//프로젝트 모델
 
+//프로젝트 모델
 @Parcelize
 data class MuckProjectModel (
+
     //프로젝트 id
-    val projectId:Int = 0,
+    val projectId:Int? = null,
 //    프로젝트명
-    val projectTitle:String = "여",
+    val projectTitle:String? = null,
 //    프로젝트 이미지
-    val projectImage:String?= "야",
+    val projectImage:String?= null,
 //    프로젝트 설명
     val projectDescription:String?=null,
 //    필요한 디자이너 수
-    val needDesignerCount:Int?=5,
+    val needDesignerCount:Int?= null,
 //    필요한 기획자 수
-    val needPlannerCount:Int?=5,
+    val needPlannerCount:Int?=null,
 //    필요한 개발자 수
-    val needDeveloperCount:Int?=5,
+    val needDeveloperCount:Int?=null,
 //    팀으로 소속된 멤버들
     val memberList:List<MuckUserData>? = null ,
 //    참가모집 현황
     val recruitmentStatus:String?=null,
 //    좋아요 갯수
-    val likeCount:Int?=5,
+    val likeCount:Int?=null,
 //    댓글 갯수
     val commentsCount:List<String>?= null,
 
     //리더 이름
-    val leaderName:String?=null,
-//    리더 프로필 사진
-    val leaderImage:String?=null,
-//    리더 직업
-    val leaderJob:String?=null,
+    val leaderData:List<MuckUserData>? = null,
+
 //    프로젝트 올린 날짜
     @ServerTimestamp
     val createAt: Timestamp? = null,
@@ -45,7 +43,7 @@ data class MuckProjectModel (
     @ServerTimestamp
     val lastUpdated: Timestamp? = null,
 
-//    태그 리스트
+//   태그 리스트
     val tagList:List<String>?=null,
 
     ):Parcelable
